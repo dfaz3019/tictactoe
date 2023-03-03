3ce99121a798a5fe701c275cc24b19964ce8607a
Dana Fazeli <danafazeli@hotmail.co.uk>
2023-03-03 23:28:18 +1100

    Added functions to all cells

--- script.js
+++ script.js
@@ -18,23 +18,124 @@ function randomNumber () {
     return roundedNumber;
 }
 
-function inputValue () {
-    if (cellOne.textContent === 'O') {
+function inputValueCellOne () {
+    if (cellOne.textContent === 'X') {
         cellOne.textContent = ''
     } else if (cellOne.textContent === 'X') {
         cellOne.textContent = 'O'
     } else if (cellOne.textContent === 'O') {
         cellOne.textContent = 'X'
     } else if (cellOne.textContent === '') {
+        cellOne.textContent = 'O'
+    }
+}
+
+function inputValueCellTwo () {
+    if (cellTwo.textContent === 'X') {
+        cellOne.textContent = ''
+    } else if (cellTwo.textContent === 'X') {
+        cellOne.textContent = 'O'
+    } else if (cellTwo.textContent === 'O') {
         cellOne.textContent = 'X'
+    } else if (cellTwo.textContent === '') {
+        cellOne.textContent = 'O'
+    }
+}
+
+function inputValueCellThree () {
+    if (cellThree.textContent === 'X') {
+        cellOne.textContent = ''
+    } else if (cellThree.textContent === 'X') {
+        cellOne.textContent = 'O'
+    } else if (cellThree.textContent === 'O') {
+        cellOne.textContent = 'X'
+    } else if (cellThree.textContent === '') {
+        cellOne.textContent = 'O'
+    }
+}
+
+function inputValueCellFour () {
+    if (cellFour.textContent === 'X') {
+        cellOne.textContent = ''
+    } else if (cellFour.textContent === 'X') {
+        cellOne.textContent = 'O'
+    } else if (cellFour.textContent === 'O') {
+        cellOne.textContent = 'X'
+    } else if (cellFour.textContent === '') {
+        cellOne.textContent = 'O'
+    }
+}
+
+function inputValueCellFive () {
+    if (cellFive.textContent === 'X') {
+        cellOne.textContent = ''
+    } else if (cellFive.textContent === 'X') {
+        cellOne.textContent = 'O'
+    } else if (cellFive.textContent === 'O') {
+        cellOne.textContent = 'X'
+    } else if (cellFive.textContent === '') {
+        cellOne.textContent = 'O'
+    }
+}
+
+function inputValueCellSix () {
+    if (cellOne.textContent === 'X') {
+        cellSix.textContent = ''
+    } else if (cellSix.textContent === 'X') {
+        cellOne.textContent = 'O'
+    } else if (cellSix.textContent === 'O') {
+        cellOne.textContent = 'X'
+    } else if (cellSix.textContent === '') {
+        cellOne.textContent = 'O'
+    }
+}
+
+function inputValueCellSeven () {
+    if (cellSeven.textContent === 'X') {
+        cellOne.textContent = ''
+    } else if (cellSeven.textContent === 'X') {
+        cellOne.textContent = 'O'
+    } else if (cellSeven.textContent === 'O') {
+        cellOne.textContent = 'X'
+    } else if (cellSeven.textContent === '') {
+        cellOne.textContent = 'O'
+    }
+}
+
+function inputValueCellEight () {
+    if (cellEight.textContent === 'X') {
+        cellOne.textContent = ''
+    } else if (cellEight.textContent === 'X') {
+        cellOne.textContent = 'O'
+    } else if (cellEight.textContent === 'O') {
+        cellOne.textContent = 'X'
+    } else if (cellEight.textContent === '') {
+        cellOne.textContent = 'O'
+    }
+}
+
+function inputValueCellNine () {
+    if (cellNine.textContent === 'X') {
+        cellOne.textContent = ''
+    } else if (cellNine.textContent === 'X') {
+        cellOne.textContent = 'O'
+    } else if (cellNine.textContent === 'O') {
+        cellOne.textContent = 'X'
+    } else if (cellNine.textContent === '') {
+        cellOne.textContent = 'O'
     }
 }
 
 
-cellOne.addEventListener('click', inputValue)
-
-
-
+cellOne.addEventListener('click', inputValueCellOne)
+cellOne.addEventListener('click', inputValueCellTwo)
+cellOne.addEventListener('click', inputValueCellThree)
+cellOne.addEventListener('click', inputValueCellFour)
+cellOne.addEventListener('click', inputValueCellFive)
+cellOne.addEventListener('click', inputValueCellSix)
+cellOne.addEventListener('click', inputValueCellSeven)
+cellOne.addEventListener('click', inputValueCellEight)
+cellOne.addEventListener('click', inputValueCellNine)
 
 /* Computer Logic
 cellOne.addEventListener('click', () => {
