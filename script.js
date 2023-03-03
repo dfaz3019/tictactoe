99c14f2a475259627c183dc3a26c84866e370c9f
Andrew Miller <andrew.william.miller7@gmail.com>
2023-03-04 01:17:55 +1300

    changed numbered cell classes to numbered cell IDs

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
-var cellOne = document.querySelector('.cell-1')
-var cellTwo = document.querySelector('.cell-2')
-var cellThree = document.querySelector('.cell-3')
-var cellFour = document.querySelector('.cell-4')
-var cellFive = document.querySelector('.cell-5')
-var cellSix = document.querySelector('.cell-6')
-var cellSeven = document.querySelector('.cell-7')
-var cellEight = document.querySelector('.cell-8')
-var cellNine = document.querySelector('.cell-9')
+var cellOne = document.querySelector('#cell-1');
+var cellTwo = document.querySelector('#cell-2');
+var cellThree = document.querySelector('#cell-3');
+var cellFour = document.querySelector('#cell-4');
+var cellFive = document.querySelector('#cell-5');
+var cellSix = document.querySelector('#cell-6');
+var cellSeven = document.querySelector('#cell-7');
+var cellEight = document.querySelector('#cell-8');
+var cellNine = document.querySelector('#cell-9');
+
 
 /* Function to generate a random number between 1-9 */
 
